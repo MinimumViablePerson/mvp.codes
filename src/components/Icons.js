ede42e8f150e0bd1ff2a653fbedbac786fad2934
MinimumViablePerson <nicolas.marcora@yahoo.co.uk>
2019-07-08 22:43:19 +0100

    flippy name and icon colors

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -4,8 +4,9 @@ import styled, { keyframes } from 'styled-components'
 import { ReactComponent as Twitter } from '../images/twitter.svg'
 import { ReactComponent as LinkedIn } from '../images/linkedin.svg'
 import { ReactComponent as Medium } from '../images/medium.svg'
+import { ReactComponent as Github } from '../images/github.svg'
 
-const animation = keyframes`
+const fadeIn = keyframes`
   0% {
     transform: translateY(100px);
     opacity: 0;
@@ -19,9 +20,10 @@ const animation = keyframes`
 
 const Icons = ({ className }) =>
   <div className={className}>
-    <a title='Twitter' rel="noopener noreferrer" target='_blank' href='https://twitter.com/NicolasMarcora'><Twitter /></a>
-    <a title='LinkedIn' rel="noopener noreferrer" target='_blank' href='https://www.linkedin.com/in/nmarcora/'><LinkedIn /></a>
-    <a title='Medium' rel="noopener noreferrer" target='_blank' href='https://medium.com/@nicolasmarcora'><Medium /></a>
+    <a id='twitter' title='Twitter' rel="noopener noreferrer" target='_blank' href='https://twitter.com/NicolasMarcora'><Twitter /></a>
+    <a id='linkedin' title='LinkedIn' rel="noopener noreferrer" target='_blank' href='https://www.linkedin.com/in/nmarcora/'><LinkedIn /></a>
+    <a id='medium' title='Medium' rel="noopener noreferrer" target='_blank' href='https://medium.com/@nicolasmarcora'><Medium /></a>
+    <a id='github' title='Github' rel="noopener noreferrer" target='_blank' href='https://github.com/MinimumViablePerson'><Github /></a>
   </div>
 
 const StyledIcons = styled(Icons)`
@@ -32,13 +34,28 @@ const StyledIcons = styled(Icons)`
     width: 60px;
     height: 60px;
     margin: 0 10px;
-    animation: 1s ${animation};
-    transition: 700ms ease-in-out;
+    animation: 1s ${fadeIn};
+    transition: 1s ease-in-out;
 
     &:hover {
-      fill: rgba(9,141,222);
-      transform: rotate(360deg);
+      transform: rotate(-10deg);
     }
+  }
+
+  #twitter svg:hover {
+    fill: #1d9deb;
+  }
+
+  #linkedin svg:hover {
+    fill: #0272af;
+  }
+
+  #medium svg:hover {
+    fill: #00d060;
+  }
+
+  #github svg:hover {
+    fill: #9d219c;
   }
 `
 
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled, { keyframes } from 'styled-components'
 
 const animation = keyframes`
@@ -13,19 +13,41 @@ const animation = keyframes`
   }
 `
 
-const Intro = ({ className }) =>
-  <div className={className}>
-    <h1>I'm <span>Nicolas Marcora</span>.</h1>
-    <h2>Also known as <span>MinimumViablePerson</span>.</h2>
+const Intro = ({ className }) => {
+  const [flipped, setFlipped] = useState(false)
+
+  const flip = () => setFlipped(!flipped)
+
+  return <div className={className}>
+    <h1 id='im'>I'm</h1>
+    <div className={`flip-container ${flipped ? 'flip' : ''}`} onClick={flip}>
+      <div className='flipper'>
+        <div className='front'>
+          <h1><span>Nicolas Marcora</span></h1>
+        </div>
+        <div className='back'>
+          <h1><span>MinimumViablePerson</span></h1>
+        </div>
+      </div>
+    </div>
   </div>
+}
 
 const StyledIntro = styled(Intro)`
   width: 100%;
   text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
 
   h1 {
-    font-size: 3rem;
+    font-size: 2.5rem;
     margin-bottom: 10px;
+  }
+
+  #im {
+    width: 100%;
   }
 
   h2 {
@@ -37,6 +59,49 @@ const StyledIntro = styled(Intro)`
   span {
     color: rgba(9,141,222);
     font-weight: 600;
+    cursor: pointer;
+  }
+
+  .flip-container {
+    perspective: 1000px;
+  }
+  /* flip the pane when hovered */
+  .flip-container.flip .flipper {
+    transform: rotateY(180deg);
+  }
+
+  .flip-container, .front, .back {
+    width: 100%;
+    height: 50px;
+  }
+
+  /* flip speed goes here */
+  .flipper {
+    transition: 0.6s;
+    transform-style: preserve-3d;
+
+    position: relative;
+  }
+
+  /* hide back of pane during swap */
+  .front, .back {
+    backface-visibility: hidden;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  /* front pane, placed above back */
+  .front {
+    z-index: 2;
+    /* for firefox 31 */
+    transform: rotateY(0deg);
+  }
+
+  /* back, initially hidden pane */
+  .back {
+    transform: rotateY(180deg);
   }
 `
 
